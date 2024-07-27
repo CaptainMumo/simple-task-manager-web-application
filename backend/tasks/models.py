a7dcae1860923e91a935bffeac133c9422fcb73f
CaptainMumo <mumopeter17@gmail.com>
2024-07-27 13:48:50 +0300

    Define tasks model

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,21 @@
+"""
+Tasks app models.
+"""
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    """
+    Task model class.
+
+    Attributes:
+        title (CharField): Title of the task.
+        description (TextField): Description of the task.
+        completed (BooleanField): Whether the task is completed or not.
+        created_at (DateTimeField): Date and time when the task was created.
+    """
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
