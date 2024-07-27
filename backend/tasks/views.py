8ef57dd8f2183549b253d7d99ef8e0178fe3a85d
CaptainMumo <mumopeter17@gmail.com>
2024-07-27 17:08:22 +0300

    Add authentication to all tasks endpoints

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -2,6 +2,7 @@
 Tasks app views.
 """
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -18,3 +19,10 @@ class TaskViewSet(viewsets.ModelViewSet):
     """
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
