0203b8233427d6d00b5390a7987d6d4c53bb6c29
CaptainMumo <mumopeter17@gmail.com>
2024-07-28 14:59:43 +0300

    Add user authentication serializers for registration and logging in

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -2,6 +2,8 @@
 Tasks app serializers.
 """
 from rest_framework import serializers
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from .models import Task
 
 
@@ -16,3 +18,72 @@ class TaskSerializer(serializers.ModelSerializer):
         """
         model = Task
         fields = "__all__"
+        read_only_fields = ["user"]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    User serializer class.
+    """
+
+    class Meta:
+        """
+        Serializer Meta class.
+        """
+        model = User
+        fields = ["id", "username"]
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    """
+    Register serializer class.
+    """
+
+    class Meta:
+        """
+        Serializer Meta class.
+        """
+        model = User
+        fields = ["username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        """
+        Create a new user with encrypted password and return it.
+        """
+        user = User.objects.create_user(
+            validated_data["username"], password=validated_data["password"]
+        )
+
+        return user
+    
+
+class LoginSerializer(serializers.Serializer):
+    """
+    Login serializer class.
+    """
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, attrs):
+        """
+        Validate and authenticate the user.
+        """
+        user = authenticate(**attrs)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid credentials")    
+    
+
+    def create(self, validated_data):
+        """
+        Implementing to adhere to abstract method requirements.
+        """
+        pass
+
+    def update(self, instance, validated_data):
+        """
+        Implementing to adhere to abstract method requirements.
+        """
+        pass
+
