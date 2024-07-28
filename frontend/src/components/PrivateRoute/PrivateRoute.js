c0cb00450c1abfb21787a8db2e4225ee5b5a332f
CaptainMumo <mumopeter17@gmail.com>
2024-07-29 01:43:48 +0300

    Define private route to protect pages that require authentication

---
+++ frontend/src/components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,12 @@
+// src/components/PrivateRoute/index.js
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+
+const PrivateRoute = ({ children }) => {
+    const token = localStorage.getItem('token');
+    return token ? children : <Navigate to="/login" />;
+};
+
+
+export default PrivateRoute;
