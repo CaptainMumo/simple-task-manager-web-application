86fa4880d448c9f3ab1204b09aafa59b8a8075ff
CaptainMumo <mumopeter17@gmail.com>
2024-07-29 01:42:33 +0300

    Hide links to protected pages for security

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -1,43 +1,66 @@
 // src/components/Navigation/Navigation.js
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { ClipboardText, House, SignIn, Plus } from 'phosphor-react';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { ClipboardText, House, SignIn, SignOut, Plus } from 'phosphor-react';
 import { LinkContainer } from 'react-router-bootstrap';
+import { logoutUser } from '../../api';
 
 const Navigation = () => {
-  return (
-    <Navbar bg="light" expand="lg" sticky="top" className="shadow-sm" collapseOnSelect>
-      <Container>
-        <Navbar.Brand href="/">
-          <ClipboardText size={32} />
-          <span className="ms-2">Task Manager</span>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto align-items-center">
-            <LinkContainer to="/">
-              <Nav.Link className="d-flex align-items-center">
-                <House size={24} />
-                <span className="ms-1">Home</span>
-              </Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/add-task">
-              <Nav.Link className="d-flex align-items-center">
-                <Plus size={24} />
-                <span className="ms-1">Add Task</span>
-              </Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <Nav.Link className="d-flex align-items-center border border-primary rounded-pill ms-2 p-2">
-                <span className="me-1">Sign In</span>
-                <SignIn size={24} />
-              </Nav.Link>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+
+    // Check if user is logged in
+    const token = localStorage.getItem('token');
+    const isLoggedIn = token !== null;
+
+    // Handle logout
+    const handleLogout = async () => {
+        await logoutUser();
+        window.location.reload();
+    }
+
+    return (
+        <Navbar bg="light" expand="lg" sticky="top" className="shadow-sm" collapseOnSelect>
+            <Container>
+                <Navbar.Brand href="/">
+                    <ClipboardText size={32} />
+                    <span className="ms-2">Task Manager</span>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto align-items-center">
+                        {isLoggedIn ? (
+                            <>
+                                <LinkContainer to="/">
+                                    <Nav.Link className="d-flex align-items-center">
+                                        <House size={24} />
+                                        <span className="ms-1">Home</span>
+                                    </Nav.Link>
+                                </LinkContainer>
+                                <LinkContainer to="/add-task">
+                                    <Nav.Link className="d-flex align-items-center">
+                                        <Plus size={24} />
+                                        <span className="ms-1">Add Task</span>
+                                    </Nav.Link>
+                                </LinkContainer>
+                                <Button variant="danger" className="bg-transparent border-0" onClick={handleLogout}>
+                                    <Nav.Link className="d-flex align-items-center">
+                                        <SignOut size={24} />
+                                        <span className="ms-1">Logout</span>
+                                    </Nav.Link>
+                                </Button>
+                            </>
+                        ) : (
+                            <LinkContainer to="/login">
+                                <Nav.Link className="d-flex align-items-center border border-primary rounded-pill ms-2 p-2">
+                                    <span className="me-1">Sign In/Sign Up</span>
+                                    <SignIn size={24} />
+                                </Nav.Link>
+                            </LinkContainer>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 };
 
 export default Navigation;
