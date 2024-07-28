733d9e5044ceec0aba46c7bec2d5c2724953ac4e
CaptainMumo <mumopeter17@gmail.com>
2024-07-28 13:05:52 +0300

    Add authentication endpoints

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,9 +1,13 @@
 """
-URL configuration for tasks app.
+URL endpoint configuration for tasks app.
 """
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from .views import (TaskViewSet, LogoutView)
 
 
 router = DefaultRouter()
@@ -11,4 +15,7 @@ router.register('tasks', TaskViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/logout/', LogoutView.as_view(), name='auth_logout'),
 ]
