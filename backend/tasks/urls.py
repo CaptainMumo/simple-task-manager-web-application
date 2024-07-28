130d3709e8a2f30df27b8a67ad026112081239b8
CaptainMumo <mumopeter17@gmail.com>
2024-07-28 15:00:57 +0300

    Define authentication endpoints

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -3,7 +3,7 @@ URL configuration for tasks app.
 """
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet
+from .views import (TaskViewSet, RegisterView, LoginView, LogoutView)
 
 
 router = DefaultRouter()
@@ -11,4 +11,7 @@ router.register('tasks', TaskViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/register', RegisterView.as_view(), name='auth_register'),
+    path('auth/login', LoginView.as_view(), name='auth_login'),
+    path('auth/logout', LogoutView.as_view(), name='auth_logout'),
 ]
