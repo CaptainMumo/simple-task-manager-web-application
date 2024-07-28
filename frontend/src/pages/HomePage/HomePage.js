b438188cd2479adcc37e370ff1930698af4550a7
CaptainMumo <mumopeter17@gmail.com>
2024-07-29 01:43:13 +0300

    Update home page with placeholder display

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,11 +1,12 @@
 // src/pages/HomePage/HomePage.js
 import React from 'react';
-import AuthForm from '../../components/Authentication';
 
 const HomePage = () => {
-  return (
-    <AuthForm />
-  )
+	return (
+		<div className='text-center justify-content-center align-items-center vh-100 d-flex'>
+			<h1>Welcome Home, Page</h1>
+		</div>
+  	)
 };
 
 export default HomePage;
