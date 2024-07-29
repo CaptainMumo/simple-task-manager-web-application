9d36d30889807aa4cfd09c0363ad25963d932251
CaptainMumo <mumopeter17@gmail.com>
2024-07-29 21:36:45 +0300

    Update authentication page

--- frontend/src/components/Authentication/AuthForm.js
+++ frontend/src/components/Authentication/AuthForm.js
@@ -1,3 +1,4 @@
+// src/components/Authentication/AuthForm.js
 import React, { useState, useEffect } from 'react';
 import { Container, Tab, Tabs, Form, Button, Spinner } from 'react-bootstrap';
 import { MdCheckCircle, MdCancel } from 'react-icons/md';
