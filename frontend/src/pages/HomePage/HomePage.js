69db0f5fcd6adb8927b0e2f5b51ff28c4cf1bed1
CaptainMumo <mumopeter17@gmail.com>
2024-07-29 12:57:29 +0300

    Add new task creation functionality

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -2,42 +2,62 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import TaskList from '../../components/TaskList';
+import NewTask from '../../components/NewTask/';
 import { getTasks, patchTask, deleteTask } from '../../api';
+import { useModal } from '../../context/ModalContext';
 
-
+const sortTasks = (tasks) => {
+    return tasks.sort((a, b) => {
+        if (a.completed && !b.completed) {
+            return 1;
+        } else if (!a.completed && b.completed) {
+            return -1;
+        } else {
+            return 0;
+        }
+    });
+}
 const HomePage = () => {
     const [tasks, setTasks] = useState([]);
+    const { openNewTaskModal, showNewTaskModal, closeNewTaskModal } = useModal();
     const navigate = useNavigate();
 
     // Fetch tasks from the server
     useEffect(() => {
-        const fetchTasks = async () => {
-            try {
-                const data = await getTasks();
-                setTasks(data);
-            } catch (error) {
-                console.error('Error fetching tasks:', error);
-            }
-        };
-
         fetchTasks();
     }, []);
 
 
+    const fetchTasks = async () => {
+        try {
+            const data = await getTasks();
+            sortTasks(data);
+            setTasks(data);
+        } catch (error) {
+            console.error('Error fetching tasks:', error);
+        }
+    };
+
+
     const handleAddTask = () => {
         // Logic to add a new task
+        openNewTaskModal();
     };
+
+    const handleTaskCreated = (newTask) => {     
+        fetchTasks();  
+        // Delay a little before closing modal
+        setTimeout(() => {
+            closeNewTaskModal();
+        }, 2000);
+    }
 
     const handleMarkComplete = async (taskId) => {
         try {
             const task = tasks.find((task) => task.id === taskId);
             const completed = !task.completed;
             const updatedTask = await patchTask(taskId, { completed });
-            setTasks((prevTasks) =>
-                prevTasks.map((task) =>
-                    task.id === taskId ? updatedTask : task
-                )
-            );
+            fetchTasks();
         } catch (error) {
             console.error('Error marking task as complete:', error);
         }
@@ -67,6 +87,11 @@ const HomePage = () => {
                 handleMarkComplete={handleMarkComplete}
                 handleEditTask={handleEditTask}
                 handleDeleteTask={handleDeleteTask}
+            />
+            <NewTask
+                show={showNewTaskModal}
+                onHide={closeNewTaskModal}
+                onTaskCreated={handleTaskCreated}
             />
         </div>
     );
