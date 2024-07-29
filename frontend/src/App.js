62c2b2a1b3886fff03ab22db5497910d75ef077e
CaptainMumo <mumopeter17@gmail.com>
2024-07-29 12:54:07 +0300

    Wrap main application with ModalProvider to allow task creation in all pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ModalProvider } from './context/ModalContext';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
@@ -12,20 +13,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
     return (
-        <Router>
-            <div className='d-flex flex-column min-vh-100'>
-                <Navigation />
-                <div className='flex-grow-1'>
-                    <Routes>
-                        <Route path="/" element={<PrivateRoute><HomePage /></PrivateRoute>} />
-                        <Route path="/login" element={<AuthPage />} />
-                        <Route path="/tasks/:id" element={<PrivateRoute><TaskDetailPage /></PrivateRoute>} />
-                    </Routes>
+        <ModalProvider>
+            <Router>
+                <div className='d-flex flex-column min-vh-100'>
+                    <Navigation />
+                    <div className='flex-grow-1'>
+                        <Routes>
+                            <Route path="/" element={<PrivateRoute><HomePage /></PrivateRoute>} />
+                            <Route path="/login" element={<AuthPage />} />
+                            <Route path="/tasks/:id" element={<PrivateRoute><TaskDetailPage /></PrivateRoute>} />
+                        </Routes>
+                    </div>
+                    <Footer />
                 </div>
-                <Footer />
-            </div>
-        </Router>
-
+            </Router>
+        </ModalProvider>
     );
 }
 
