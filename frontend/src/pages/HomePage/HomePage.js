da9fdb59bbb5c7c83d6d2eb5404922b32507bf4d
CaptainMumo <mumopeter17@gmail.com>
2024-07-29 21:40:07 +0300

    Add loading state and error handling to home page

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -5,6 +5,9 @@ import TaskList from '../../components/TaskList';
 import NewTask from '../../components/NewTask/';
 import { getTasks, patchTask, deleteTask } from '../../api';
 import { useModal } from '../../context/ModalContext';
+import { Button, Spinner } from 'react-bootstrap';
+import CustomModal from '../../components/Modal/CustomModal';
+import { MdCancel, MdCheckCircle } from 'react-icons/md';
 
 const sortTasks = (tasks) => {
     return tasks.sort((a, b) => {
@@ -17,9 +20,20 @@ const sortTasks = (tasks) => {
         }
     });
 }
+
 const HomePage = () => {
-    const [tasks, setTasks] = useState([]);
     const { openNewTaskModal, showNewTaskModal, closeNewTaskModal } = useModal();
+
+    const [tasks, setTasks] = useState([]);
+    const [showConfirmDeleteModal, setShowConfirmDeleteModal] = useState(false);
+    const [showMessageModal, setShowMessageModal] = useState(false);
+    const [modalTitle, setModalTitle] = useState('');
+    const [modalMessage, setModalMessage] = useState('');
+    const [isError, setIsError] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [taskIdToDelete, setTaskIdToDelete] = useState(null);
+    const timeOut = 3000;
+
     const navigate = useNavigate();
 
     // Fetch tasks from the server
@@ -27,71 +41,144 @@ const HomePage = () => {
         fetchTasks();
     }, []);
 
-
     const fetchTasks = async () => {
+        setLoading(true);
         try {
             const data = await getTasks();
             sortTasks(data);
             setTasks(data);
         } catch (error) {
-            console.error('Error fetching tasks:', error);
+            setModalTitle('Error fetching tasks');
+            setModalMessage('An error occurred while fetching tasks. Please try again later.');
+            setIsError(true);
+            setShowMessageModal(true);
+        } finally {
+            setLoading(false);
         }
     };
 
-
     const handleAddTask = () => {
-        // Logic to add a new task
         openNewTaskModal();
     };
 
-    const handleTaskCreated = (newTask) => {     
-        fetchTasks();  
+    const handleTaskCreated = () => {
+        fetchTasks();
         // Delay a little before closing modal
         setTimeout(() => {
             closeNewTaskModal();
-        }, 2000);
+        }, timeOut);
     }
 
     const handleMarkComplete = async (taskId) => {
+        setLoading(true);
         try {
             const task = tasks.find((task) => task.id === taskId);
             const completed = !task.completed;
-            const updatedTask = await patchTask(taskId, { completed });
-            fetchTasks();
+            const patchedTask = await patchTask(taskId, { completed });
+            setTasks((prevTasks) => prevTasks.map((t) => (t.id === taskId ? patchedTask : t)));
+            setModalTitle('Success');
+            setModalMessage('Task updated successfully!');
+            setIsError(false);
         } catch (error) {
+            setModalTitle('Error');
+            setModalMessage('Error marking task as complete.');
+            setIsError(true);
             console.error('Error marking task as complete:', error);
+        } finally {
+            setLoading(false);
+            setShowMessageModal(true);
         }
     };
 
     const handleEditTask = (taskId) => {
-        // Logic to edit a task
         navigate(`/tasks/${taskId}`);
     };
 
-    const handleDeleteTask = async (taskId) => {
-        if (window.confirm('Are you sure you want to delete this task?')) {
-            try {
-                await deleteTask(taskId);
-                setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
-            } catch (error) {
-                console.error('Error deleting task:', error);
-            }
+    const handleDeleteTask = (taskId) => {
+        setTaskIdToDelete(taskId);
+        setShowConfirmDeleteModal(true);
+    };
+
+    const confirmDelete = async () => {
+        setLoading(true);
+        try {
+            await deleteTask(taskIdToDelete);
+            setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskIdToDelete));
+            setModalTitle('Success');
+            setModalMessage('Task deleted successfully!');
+            setIsError(false);
+        } catch (error) {
+            setModalTitle('Error');
+            setModalMessage('Error deleting task.');
+            setIsError(true);
+            console.error('Error deleting task:', error);
+        } finally {
+            setLoading(false);
+            setShowConfirmDeleteModal(false);
+            setShowMessageModal(true);
         }
     };
 
     return (
         <div>
-            <TaskList
-                tasks={tasks}
-                handleAddTask={handleAddTask}
-                handleMarkComplete={handleMarkComplete}
-                handleEditTask={handleEditTask}
-                handleDeleteTask={handleDeleteTask}
-            />
+            {loading ? (
+                <div className="d-flex justify-content-center align-items-center vh-100">
+                    <Spinner animation="border" size="lg" />
+                </div>
+            ) : (
+                <TaskList
+                    tasks={tasks}
+                    handleAddTask={handleAddTask}
+                    handleMarkComplete={handleMarkComplete}
+                    handleEditTask={handleEditTask}
+                    handleDeleteTask={handleDeleteTask}
+                />
+            )}
+
             <NewTask
                 show={showNewTaskModal}
                 onHide={closeNewTaskModal}
                 onTaskCreated={handleTaskCreated}
+            />
+            <CustomModal
+                show={showMessageModal}
+                onHide={() => setShowMessageModal(false)}
+                title={modalTitle}
+                body={
+                    <div className='text-center align-items-center justify-content-center d-flex'>
+                        {loading ? (
+                            <>
+                                <Spinner animation="border" size="lg" />
+                                <div className="mt-2">Please wait...</div>
+                            </>
+                        ) : isError ? (
+                            <div>
+                                <MdCancel size={48} color="red" />
+                                <div className="text-danger">{modalMessage}</div>
+                            </div>
+                        ) : (
+                            <div>
+                                <MdCheckCircle size={48} color="green" />
+                                <div className="text-success">{modalMessage}</div>
+                            </div>
+                        )}
+                    </div>
+                }
+                autoClose={!isError}
+                autoCloseTime={timeOut}
+            />
+
+            <CustomModal
+                show={showConfirmDeleteModal}
+                onHide={() => setShowConfirmDeleteModal(false)}
+                title="Confirm Delete"
+                body="Are you sure you want to delete this task?"
+                footer={
+                    <>
+                        <Button variant="secondary" onClick={() => setShowConfirmDeleteModal(false)}>Cancel</Button>
+                        <Button variant="danger" onClick={confirmDelete}>Delete</Button>
+                    </>
+                }
             />
         </div>
     );
