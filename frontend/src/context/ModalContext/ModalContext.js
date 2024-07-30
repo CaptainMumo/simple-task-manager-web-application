1713ae9ecb1d4fad26afa4f489a92964cd317799
CaptainMumo <mumopeter17@gmail.com>
2024-07-30 15:29:05 +0300

    Remove unused import

--- frontend/src/context/ModalContext/ModalContext.js
+++ frontend/src/context/ModalContext/ModalContext.js
@@ -1,5 +1,5 @@
 // src/context/ModalContext/ModalContext.js
-import React, { createContext, useContext } from 'react';
+import { createContext, useContext } from 'react';
 
 const ModalContext = createContext();
 
