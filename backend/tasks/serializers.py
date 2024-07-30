aa1bc3b972ecadaf4338ecb42915fbdaf52cda3c
CaptainMumo <mumopeter17@gmail.com>
2024-07-30 15:26:51 +0300

    Remove unused import

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -2,7 +2,6 @@
 Tasks app serializers.
 """
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
 from rest_framework import serializers
 from .models import Task
 
